24260cea7276404eec64978a13e9780d684f62d0
Chris Sanders <chrisasanders@protonmail.com>
2026-06-06 14:49:28 -0400

    Updated master to main

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,29 +6,36 @@ function Contact() {
     <div className="flex flex-col mb-10 mx-auto">
       <div className="flex justify-center items-center">
         <form
-          // action="Put form link here"
+          action="https://getform.io/f/42bc9b52-ddab-4fde-94a5-b32c378a7ace"
           method="POST"
           className="flex flex-col w-full md:w-7/12"
         >
           <Title>Contact</Title>
+          
           <input
             type="text"
             name="name"
             placeholder="Name"
             className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
+            required
           />
+          
           <input
-            type="text"
+            type="email"
             name="email"
             placeholder="Email"
             className="my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none"
+            required
           />
+          
           <textarea
             name="message"
             placeholder="Message"
             rows="10"
             className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
+            required
           />
+          
           <button
             type="submit"
             className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-black drop-shadow-md hover:bg-black/60 transition-all"
