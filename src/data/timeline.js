d258009b7542f059e66fc1c6c378dd5f0200df9a
Chris Sanders <chrisasanders@protonmail.com>
2026-06-28 19:12:46 -0400

    Updated Timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,32 +1,45 @@
 export default [
   {
-    year: '2025 - Present',
+    year: 'Jun 2026 - Present',
+    title: 'Freelance QA & UAT Tester:',
+    duration: '1 month -',
+    details:
+      'Detail-oriented QA & UAT Test Specialist with hands on experience evaluating web and mobile applications for global brands.',
+  },
+  {
+    year: 'Jan 2026 - Present',
     title: 'Attending Maestro College:',
     duration: '2 years -',
     details:
-      'Earning an Associates degree in AI Engineering. Graduation Dec 2027.',
+      'Earning an Associates degree in AI Software Engineering. Graduation Dec 2027.',
   },
   {
     year: '2023 - 2025',
     title: 'Web Developer:',
     duration: '3 years -',
     details:
-      'The advancements in technology are inspiring and peaked my intellectual interest, so I achieved a personal SMART goal to: Successfully enter the technology field by earning a Full Stack Web Developer Certificate from Zero to Mastery along with other certificates.',
+      'The advancements in technology peaked my intellectual interest, so I achieved a personal SMART goal to: Successfully enter the technology field by earning a Full Stack Web Developer Certificate from Zero to Mastery along with other certificates.',
   },
   {
-    year: '2020 - 2023',
+    year: 'Jul 2020 - Jul 2024',
     title: 'Business Owner/Mobile Mechanic:',
     duration: '4 years -',
     details:
-      'Repair King - Automotive mechanic business. Independent Contractor for Your Mechanic Mobile automotive mechanic.',
+      'Repair King LLC - Mobile mechanic business owner and independent contractor .',
   },
-
   {
-    year: '2014 - 2019',
-    title: 'Web Developer:',
-    duration: '6 years -',
+    year: 'Aug 2020 - Dec 2023',
+    title: 'Mobile Mechanic:',
+    duration: '3 years 5 mos -',
     details:
-      'Expanding knowledge and implementation of coding and eventually built my first website for an independent musician. Wrote and published an eBook titled "How to make Beats", detailing the technical breakdown of music production.',
+      'Your Mechanic Inc - I diagnosed and repaired customers vehicles as an indenpendent contractor.',
+  },
+  {
+    year: '2014 - 2020',
+    title: 'Web Developer:',
+    duration: '7 years -',
+    details:
+      'Expanding knowledge and implementation of coding and eventually built my first website for an independent musician. I worked with many clients building my coding experience while improving my customer service skills. ',
   },
 
   {
@@ -34,11 +47,12 @@ export default [
     title: 'Automotive Technician:',
     duration: '6 years -',
     details:
-      'ASE certified technician for dealerships using the latest technology for the automotive sector.',
+      'ASE certified technician for car dealerships using the latest technology for the automotive sector. Industry Experience - Dobbs Tire & Auto Center - Firestone - Mitsubishi/Chevrolet.',
+      
   },
 
   {
-    year: '2007 - 2009',
+    year: 'Aug 2007 - May 2009',
     title: 'Automotive Technician College:',
     duration: '2 years -',
     details:
